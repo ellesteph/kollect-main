0a104593e29258664c60c1c5bb2ea9494f6e1f37
ellesteph <ellesteph2@gmail.com>
2026-03-24 19:10:07 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
-// firebase.js
 const firebaseConfig = {
-  apiKey: "AIzaSyBLN0O36j1DWxUN5U1B4iuP6kH-YNUENkk",
+  apiKey: "PASTE_NEW_KEY_HERE",
   authDomain: "kollect-aa5a3.firebaseapp.com",
   projectId: "kollect-aa5a3",
-  storageBucket: "kollect-aa5a3.firebasestorage.app",
-  messagingSenderId: "57011462366",
-  appId: "1:57011462366:web:f32cee212f055e571ae24f",
-  measurementId: "G-67DGZE48VZ"
+  storageBucket: "kollect-aa5a3.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
 firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
